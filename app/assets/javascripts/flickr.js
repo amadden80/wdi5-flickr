15f14e3b03c356e506f4f8bc3a524e09d0becb3e
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-21 14:53:58 -0400

    Fixing json_results assignment to be the object not the request

--- app/assets/javascripts/flickr.js
+++ app/assets/javascripts/flickr.js
@@ -6,11 +6,16 @@ var json_results;
 function search_flickr(){
   search = $('#search').val();
   var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4c6e1f137bbeb666a1c0a884b609e286&text=' + search + '&per_page=' + per_page + '&page=' + page + '&format=json&jsoncallback=?'
-  json_results = $.getJSON(url,
+  $.getJSON(url,
     function(data){
-      console.log(data);
+      json_results = data;
     }
   );
+}
+
+
+function get_photo_src(index){
+
 }
 
 //jQuery alternative to $(document).ready(function(){}); that is prettier (less verbose)
@@ -19,3 +24,11 @@ $(function(){
   $('#search-btn').on('click', search_flickr);
 
 });
+
+
+
+
+
+
+
+

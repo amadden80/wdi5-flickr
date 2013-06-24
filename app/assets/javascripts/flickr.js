1aa2e21c7b34d5dd55e640d632fa6ee4a6a9d2fb
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-24 01:30:58 -0400

    Better commenting

--- app/assets/javascripts/flickr.js
+++ app/assets/javascripts/flickr.js
@@ -3,14 +3,16 @@ var per_page = 20;
 var page = 1;
 var json_photos;
 
+//Called when the search button is pressed. Performs an AJAX request for JSON from the Flickr API, returning photos that match the search field's query
 function search_flickr(){
   search = $('#search').val();
   var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4c6e1f137bbeb666a1c0a884b609e286&text=' + search + '&per_page=' + per_page + '&page=' + page + '&format=json&jsoncallback=?';
   $.getJSON(url,
     function(data){
       json_photos = data.photos.photo;
+      add_photos();
     }
-  ).done(add_photos);
+  );
 }
 
 //Loops and adds each photo in the returned photos
@@ -20,12 +22,14 @@ function add_photos(){
   }
 }
 
+//Creates a photo corresponding to a returned photo
 function add_photo(index){
   var image = $('<img>');
   image.attr('src', get_photo_src(index));
   $('#photo-bag').append(image);
 }
 
+// Parses the JSON object of a photo and returns a complicated concatenated string for the src URL of that photo
 function get_photo_src(index){
   var photo = json_photos[index];
   var src = "http://farm"+ photo.farm +".static.flickr.com/"+ photo.server +"/"+ photo.id +"_"+ photo.secret +"_m.jpg";
